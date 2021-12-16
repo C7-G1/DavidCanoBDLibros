aabb01203f1fd764a1cfb5558ba45339daec00b9
Draco32170 <canodavid498@gmail.com>
2021-12-16 13:56:19 +0100

    Mirar el metodo consultar y borrar y por ultimo terminar el de listar

--- app/src/main/java/david/cano/davidcanobdlibros/MainActivity.java
+++ app/src/main/java/david/cano/davidcanobdlibros/MainActivity.java
@@ -15,16 +15,16 @@ public class MainActivity extends AppCompatActivity {
     EditText edcodigo;
     EditText edtitulo;
     EditText edautor;
-    Button btLimpiar;
-    CrearBD crearBD=new CrearBD(this);
+    CrearBD crearBD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText edcodigo=(EditText) findViewById(R.id.etCodigo);
-        EditText edtitulo=(EditText) findViewById(R.id.etTitulo);
-        EditText edautor=(EditText) findViewById(R.id.etAutor);
-        SQLiteDatabase bd= crearBD.getWritableDatabase();
+        crearBD = new CrearBD(this);
+        edcodigo=findViewById(R.id.etCodigo);
+        edtitulo=findViewById(R.id.etTitulo);
+        edautor=findViewById(R.id.etAutor);
         /*bd.execSQL("INSERT INTO libros VALUES(1,'La Celestina', 'Fernando de Rojas');");
         bd.execSQL("INSERT INTO libros VALUES(2,'El Quijote', 'Miguel de Cervantes');");
         bd.execSQL("INSERT INTO libros VALUES(3,'El Lazarillo de Tormes','Anonimo');");
@@ -34,69 +34,73 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    public void ConsultarLibro(View v)
-    {
-        String cod=edcodigo.getText().toString();
-        SQLiteDatabase bd = crearBD.getReadableDatabase();
-        Cursor contenido = bd.rawQuery("select * from libros where codigo='" +cod+ "'" , null);
-        if (contenido.moveToNext()){
+    public void ConsultarLibro(View v) {
+        String cod = edcodigo.getText().toString();
+        SQLiteDatabase bd;
+        bd = crearBD.getReadableDatabase();
+        Cursor contenido = bd.rawQuery("select * from libros where codigo='" + cod + "';", null);
+        if (contenido.moveToNext()) {
             edtitulo.setText(contenido.getString(1));
-            edautor.setText(contenido.getString(2));  }
+            edautor.setText(contenido.getString(2));
+        }
         contenido.close();
         bd.close();
     }
 
-    public void insertarLibro(View v){
-        SQLiteDatabase bd=crearBD.getWritableDatabase();
-        if(edcodigo.getText().toString().equals("")|| edtitulo.getText().toString().equals("")||
-        edautor.getText().toString().equals("")){
-        verMensajeToast("Cajas vacías, debes introducir los datos");
-        }else{
-            String cod=edcodigo.getText().toString();
-            String tit=edtitulo.getText().toString();
-            String aut=edautor.getText().toString();
-            try{
-                bd.execSQL("INSERT INTO LIBROS VALUES("+cod + ",'" + tit + ",'" + aut +"'); ");
+    public void insertarLibro(View v) {
+        SQLiteDatabase bd;
+        bd = crearBD.getWritableDatabase();
+
+        if (edcodigo.getText().toString().equals("") || edtitulo.getText().toString().equals("") ||
+                edautor.getText().toString().equals("")) {
+            verMensajeToast("Cajas vacías, debes introducir los datos");
+        } else {
+            String cod = edcodigo.getText().toString();
+            String tit = edtitulo.getText().toString();
+            String aut = edautor.getText().toString();
+            try {
+                bd.execSQL("INSERT INTO libros VALUES('" + cod + "','" + tit + "','" + aut + "');");
                 verMensajeToast("Datos insertados");
-            }catch(Exception sqlex){
+            } catch (Exception sqlex) {
+                verMensajeToast(sqlex.getMessage());
+            }
+        }
+        bd.close();
+        limpiarCajas();
+    }
+
+    public void verMensajeToast(String mensaje) {
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, mensaje, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void limpiarCajas() {
+        edcodigo.setText("");
+        edtitulo.setText("");
+        edautor.setText("");
+    }
+
+    public void borrarLibro(View v) {
+        SQLiteDatabase bd;
+
+        bd = crearBD.getWritableDatabase();
+        if (edcodigo.getText().toString().equals("") || edtitulo.getText().toString().equals("") ||
+                edautor.getText().toString().equals("")) {
+            verMensajeToast("Cajas vacías, debes introducir los datos");
+        } else {
+            String cod = edcodigo.getText().toString();
+            String tit = edtitulo.getText().toString();
+            String aut = edautor.getText().toString();
+            try {
+                bd.execSQL("DELETE INTO LIBROS VALUES(" + cod + ",'" + tit + ",'" + aut + "'); ");
+                verMensajeToast("Datos borrados");
+            } catch (Exception sqlex) {
                 verMensajeToast(sqlex.getMessage());
             }
             limpiarCajas();
             crearBD.close();
         }
     }
-
-    public void verMensajeToast(String mensaje){
-        Context context=getApplicationContext();
-        Toast toast= Toast.makeText(context,mensaje,Toast.LENGTH_LONG);
-        toast.show();
-    }
-
-    public void limpiarCajas(){
-            edcodigo.setText("");
-            edtitulo.setText("");
-            edautor.setText("");
-        }
-
-        public void borrarLibro(){
-            SQLiteDatabase bd=crearBD.getWritableDatabase();
-            if(edcodigo.getText().toString().equals("")|| edtitulo.getText().toString().equals("")||
-                    edautor.getText().toString().equals("")) {
-                verMensajeToast("Cajas vacías, debes introducir los datos");
-            }else{
-                String cod=edcodigo.getText().toString();
-                String tit=edtitulo.getText().toString();
-                String aut=edautor.getText().toString();
-                try{
-                    bd.execSQL("DELETE INTO LIBROS VALUES("+cod + ",'" + tit + ",'" + aut +"'); ");
-                    verMensajeToast("Datos borrados");
-                }catch(Exception sqlex){
-                    verMensajeToast(sqlex.getMessage());
-                }
-                limpiarCajas();
-                crearBD.close();
-            }
-        }
-    }
+}
 
--- app/src/main/java/david/cano/davidcanobdlibros/verLibros.java
+++ app/src/main/java/david/cano/davidcanobdlibros/verLibros.java
@@ -2,13 +2,46 @@ package david.cano.davidcanobdlibros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class verLibros extends AppCompatActivity {
+    CrearBD crearBD = new CrearBD(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_libros);
     }
+
+    public void consultarLibros(View v) {
+
+        List<String> items = new ArrayList<String>();
+        SQLiteDatabase bd = crearBD.getReadableDatabase();
+        Cursor contenido = bd.rawQuery("select * from libros", null);
+        int i = 0;
+        String cad = "";
+        //Volcamos el contenido del cursor en el arrayList
+        while (contenido.moveToNext() && i < contenido.getCount()) ;
+        //concatena los datos de cada libro en un String cad
+        cad = "" + contenido.getString(0) + " " + contenido.getString(1) + " " + contenido.getString(2) + "\n";
+        items.add(cad);
+        i++;
+
+
+        //Define un ArrayAdapter y volcamos en el contenido del ArrayList items
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        //llenamos el ListView listaLibros
+        //listaLibros.setAdapter(adapter);
+        contenido.close();
+        bd.close();
+    }
+
 }
